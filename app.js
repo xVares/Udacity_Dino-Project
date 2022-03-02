23bdf546249bcb8fb437226e2f13945a9803cb7c
Musti <96733958+xVares@users.noreply.github.com>
2022-03-02 06:49:09 +0100

    feat: correct innerHTML display and others
    changes made:
    
    - adjusted humanObject function
    
    - created first lines of code for compare methods
    
    - added if statements in eventListener for correct display of innerHTML

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // import dino.json 
 
 async function dinoArray() {
-    const dinoJson = await fetch("./dino.json");
-    const data = await dinoJson.json();
 
-    return data.Dinos;
+  const dinoJson = await fetch("./dino.json");
+  const data = await dinoJson.json();
+
+  return data.Dinos;
 
 }
 
@@ -14,53 +15,62 @@ async function dinoArray() {
 // Create Dino Constructor
 
 class Dinosaur {
-    constructor(species, weight, height, diet, where, when, fact) {
 
-        this.species = species;
-        this.weight = weight;
-        this.height = height;
-        this.diet = diet;
-        this.where = where;
-        this.when = when;
-        this.fact = fact;
-    }
+  constructor(species, weight, height, diet, where, when, fact) {
+
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+  }
 }
 
 
 // Create Human Object
 
-//! TODO: values are not retrieved by .value method
 class Human {
-    constructor(species, height, weight, diet) {
 
-        this.species = species;
-        this.height = height;
-        this.weight = weight;
-        this.diet = diet;
-    }
+  constructor(species, height, weight, diet) {
+
+    this.species = species;
+    this.height = height;
+    this.weight = weight;
+    this.diet = diet;
+
+  }
 }
 
-// Use IIFE to get human data from form
-
+// Function to get human data from form
 const humanObject = (function () {
 
-    let humanSpecies = document.getElementById("name").value;
-    let humanHeight = document.getElementById("feet").value * 12 + document.getElementById("inches").value;
-    let humanWeight = document.getElementById("weight").value;
-    let humanDiet = document.getElementById("diet").value;
+  let species = document.getElementById("name").value;
+  let height = document.getElementById("feet").value * 12 + document.getElementById("inches").value;
+  let weight = document.getElementById("weight").value;
+  let diet = document.getElementById("diet").value;
 
-    return new Human(humanSpecies, humanHeight, humanWeight, humanDiet)
+  return new Human(species, height, weight, diet)
 
-})();
+});
 
 // Create Dino Compare Method 1
 // TODO: compare height. 
+let compareHeight = (dinoHeight, humanHeight) => {
 
+  const ratio = Math.round(dinoHeight / humanHeight)
+
+}
 
 
 // Create Dino Compare Method 2
 // TODO: compare weight. 
+let compareWeight = (dinoWeight, humanWeight) => {
 
+  const ratio = Math.round(dinoWeight / humanWeight)
+
+}
 
 
 // Create Dino Compare Method 3
@@ -68,62 +78,63 @@ const humanObject = (function () {
 
 
 
-// TODO: Generate Tiles for each Dino in Array
-
-
-
-// TODO: Add tiles to DOM
-
-
-
-// Remove form from screen
-
-
-
 // On button click, prepare and display info graphic
 
 document.getElementById("btn").addEventListener("click", () => {
 
-    document.getElementById("dino-compare").style.display = "none";
+  // Remove form from screen
+  document.getElementById("dino-compare").style.display = "none";
 
-    const gridContainer = document.getElementById("grid");
-
-    // iterate over dinoArray with forEach
-    dinoArray().then(res => {
-        let dinos = res.map(dino => new Dinosaur(dino.species, dino.weight, dino.height, dino.diet, dino.where, dino.when, dino.fact));
-
-        dinos.splice(4, 0, humanObject);
-
-        console.log(dinos)
-
-        dinos.forEach((element, index) => {
-
-            // create elements for gridItem
-            const gridItem = document.createElement("div");
-            const gridItemHeader = document.createElement("h3");
-            const gridItemImage = document.createElement("img");
-            const gridItemParagraph = document.createElement("p");
+  const gridContainer = document.getElementById("grid");
 
 
-            // set inner HTML & Attributes for elements: (<h3>, <img>, <p>) inside the gridItem
-            gridItemHeader.innerHTML = element.species;
-            // gridItemImage.setAttribute("src", `./images/${element.species}.png`);
-            gridItemParagraph.innerHTML = element.fact;
+  // iterate over dinoArray with forEach
+  dinoArray().then(res => {
 
+    let dinos = res.map(dino => new Dinosaur(dino.species, dino.weight, dino.height, dino.diet, dino.where, dino.when, dino.fact));
 
+    // splice new "humanObject" so it gets displayed in the mid
+    dinos.splice(4, 0, humanObject());
 
-            // assign class for gridItem
-            gridItem.classList.add("grid-item");
+    dinos.forEach((element, index) => {
 
-            // append child on gridItem --> (gridItem + <h3>, <img>, <p>)
-            gridItem.appendChild(gridItemHeader);
-            gridItem.appendChild(gridItemImage);
-            gridItem.appendChild(gridItemParagraph);
+      // create elements for "gridItem"
+      const gridItem = document.createElement("div");
+      const gridItemHeader = document.createElement("h3");
+      const gridItemImage = document.createElement("img");
+      const gridItemParagraph = document.createElement("p");
 
-            gridContainer.appendChild(gridItem);
+      // set inner HTML & Attr for dinos: (<h3>, <img>, <p>) inside the "gridItem"
+      gridItemHeader.innerHTML = element.species;
+      gridItemImage.setAttribute("src", `./images/${element.species}.png`);
+      gridItemParagraph.innerHTML = element.fact;
 
-        });
-    })
+      // set inner innerHTML & Attr for "humanObject" and innerHTML for "Pigeon"
+      if (index === 4) {
 
+        gridItemImage.setAttribute("src", `./images/human.png`);
+        gridItemParagraph.innerHTML = "You!";
+
+      }
+
+      if (element.species === "Pigeon") {
+
+        gridItemParagraph.innerHTML = "All birds are Dinosaurs";
+        console.log(element);
+
+      }
+
+      // assign class for gridItem
+      gridItem.classList.add("grid-item");
+
+      // append elements on "gridItem" = (gridItem + <h3>, <img>, <p>)
+      gridItem.appendChild(gridItemHeader);
+      gridItem.appendChild(gridItemImage);
+      gridItem.appendChild(gridItemParagraph);
+
+      gridContainer.appendChild(gridItem);
+
+    });
+  })
 
 })
